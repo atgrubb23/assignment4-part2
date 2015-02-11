d64bc8ebb02cee2e3d6ee16bef360b95afdd71ef
Alan Grubb <grubba@flip2.engr.oregonstate.edu>
2015-02-10 18:12:24 -0800

    Got input validation for Add video parameters implemented.

--- videostoreInterface.php
+++ videostoreInterface.php
@@ -6,6 +6,22 @@ include('userInfo.php');
 	<head>
 		<meta charset='UTF-8'/>
 		<title>Videostore Interface</title>
+		<style>
+		fieldset {
+		display: inline-block;
+		}
+		label {
+		display: block;
+		}
+		input {
+		display: block;
+		margin: 6px;
+		}
+		input[type="submit"] {
+		position: relative;
+		left: 25%;
+		}
+		</style>
 	</head>
 
 	</body>
@@ -14,7 +30,7 @@ include('userInfo.php');
 		$vsMysqli = new mysqli($myHost, $myUsername, $myPassword, 
    			$myDatabase);
 		if(!$vsMysqli || $vsMysqli->connect_errno) {
-			echo "Could error trying to connect to " . $myHost . "."
+			echo "Error trying to connect to " . $myHost . "."
 			   . $vsMysqli->connect_errno . " " . 
 			   $vsMysqli->connect_error;
 		}
@@ -22,5 +38,20 @@ include('userInfo.php');
 			echo "Connected to " . $myHost . " successfully.";
 		}
 		?>
+		<div>
+			<form action='vsInputVal.php' method='GET'>
+			<fieldset>
+			<legend>Add Video</legend>
+				<label for='videoName'>Enter video name: </label>	
+				<input type='text' id='videoName' name='videoName'/>
+				<label for='videoCategory'>Enter video category: </label>
+				<input type='text' id='videoCategory' name='videoCategory'/>
+				<label for='videoLength'>Enter video length: </label>
+				<input type='text' id='videoLength' name='videoLength'/> 
+				<input type='submit' Submit!/>
+			</fieldset>
+			</form>
+		</div>
+		
 	</body>
 </html>
---
+++ vsInputVal.php
@@ -0,0 +1,58 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+	   $failedParams = array();
+
+	   function isInt($string) {
+		   if(!is_numeric($string) || strpbrk($string, '.')) {
+			   return false;
+		   }
+		   return true;
+	   }
+	   function isValidString($string) {
+	      if(strlen($string) > 255) {
+	      	$failedParams[] = "<p>Entry cannot exceed 255 characters.";
+		return false;
+	      }
+		return(!is_numeric($string));
+	   }
+	   
+	   if(isset($_GET['videoName'])) {
+	      $name = $_GET['videoName'];
+	      $name = explode("+", $name);
+	      $name = implode($name);
+	      if(!isValidString($name) || trim($name) === '') {
+		 $failedParams[] = "<p>The video name must be a string value and 
+		    not null.";
+	      }
+	   }
+
+	   if(isset($_GET['videoCategory'])) {
+	      if(!isValidString($_GET['videoCategory'])) {
+		 $failedParams[] = "<p>The video category must be a string value.
+		    ";
+	      }
+	   }
+
+	   if(isset($_GET['videoLength'])) {
+	      if(!isInt($_GET['videoLength'])) {
+		 $failedParams[] = "<p>The video length must be an integer value 
+		    in minutes.";
+	      }		   
+	   }
+	   //echo "<p>name = $name";
+	   //echo "<p>category = " . $_GET['videoCategory'];
+	   //echo "<p>length = " . $_GET['videoLength'];
+
+	   if(!empty($failedParams)) {
+		   foreach($failedParams as $value) {
+		      echo "<p>$value";
+		   }
+		   exit();
+	   }
+	   else {
+	     //echo "<p>Parameters validated successfully.";
+	   }
+	   $getParams = "?videoName=" . $name . "&videoCategory=" . $_GET['videoCategory'] . "&videoLength=" . $_GET['videoLength'];
+	   header('Location: http://web.engr.oregonstate.edu/~grubba/cs290/assignment4-pt2/videostoreInterface.php' . $getParams);
+   ?>
